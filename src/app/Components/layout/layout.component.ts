1ee92a41acc08d6b3b0be603a77a3270cc3a624f
haroldBauti <haroldbautistaa14@gmail.com>
2025-07-06 04:46:38 -0500

    layout actualizado

--- src/app/Components/layout/layout.component.ts
+++ src/app/Components/layout/layout.component.ts
@@ -2,15 +2,52 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../Reutilizable/shared/shared.module';
 import { NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Menu } from '../../Interfaces/menu';
+import { MenuService } from '../../Services/menu.service';
+import { UtilidadService } from '../../Reutilizable/utilidad.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [RouterModule,CommonModule,SharedModule],
+  providers:[MenuService],
   templateUrl:'./layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  listaMenus:Menu[]=[];
+  correoUsuario:string='';
+  rolUsuario:string='';
 
+  constructor(private router:Router,
+    private _menuServicio:MenuService,
+    private _utilidadServicio:UtilidadService
+  ){
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const usuario=this._utilidadServicio.ObtenerSesionUsuario();
+
+    if(usuario!=null){
+      this.correoUsuario=usuario.correo;
+      this.rolUsuario=usuario.descripcionRol;
+
+      this._menuServicio.Lista(usuario.idUsuario).subscribe({
+        next:(data)=>{
+          if(data.status){ 
+            this.listaMenus=data.value;
+            console.log(this.listaMenus);
+          }
+        },error:(e)=>{}
+      });
+    }
+  }
+  CerrarSesion(){
+    this._utilidadServicio.EliminarSesionUsuario();
+    this.router.navigate(['login']);
+  }
 }
