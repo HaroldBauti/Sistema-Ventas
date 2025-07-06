2f6b2382b8a1778d2415aec2256e6e3907643282
haroldBauti <haroldbautistaa14@gmail.com>
2025-07-05 22:53:32 -0500

    componente modal-detalle-venta agregado y archivo .ts codificado

---
+++ src/app/Components/layout/Modales/modal-detalle-venta/modal-detalle-venta.component.ts
@@ -0,0 +1,30 @@
+import { Component,Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Venta } from '../../../../Interfaces/venta';
+import { DetalleVenta } from '../../../../Interfaces/detalle-venta';
+
+
+@Component({
+  selector: 'app-modal-detalle-venta',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-detalle-venta.component.html',
+  styleUrl: './modal-detalle-venta.component.css'
+})
+export class ModalDetalleVentaComponent {
+
+  fechaRegistro:string="";
+  numeroDocumento:string="";
+  tipoPago:string="";
+  total:string="";
+  detalleVenta:DetalleVenta[]=[];
+  columnasTabla:string[]=['producto','cantidad','precio','total'];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public _venta:Venta){
+    this.fechaRegistro=_venta.fechaRegistro!;// con ! no va hacer nulo
+    this.numeroDocumento=_venta.numeroDocumento!;
+    this.tipoPago=_venta.tipoPago;
+    this.total=_venta.totalTexto;
+    this.detalleVenta=_venta.detalleVenta;
+  }
+}
